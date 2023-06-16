ca783a08c7e2cdae1b7dc4bbb77cdb38f201b009
xramzesx <xramzesbusiness@gmail.com>
2023-06-16 23:36:05 +0200

    [server] cleanup server

--- server/server.js
+++ server/server.js
@@ -6,65 +6,25 @@ const express = require('express')
 const app     = express()
 const PORT    = process.env.PORT ?? 3000
 
-console.log(models)
+const routes = require('routes')
+const utils = require('utils')
 
-const db = mongoose.connect(
-    process.env.MONGO_URL, {
-    "auth": {
-      "authSource": process.env.MONGO_ADMIN_USERNAME
-    },
-    "user": process.env.MONGO_USERNAME,
-    "pass": process.env.MONGO_PASSWORD
-  }
-).then(() => console.log(`successfully connected to ${process.env.MONGO_URL}`))
+//// SETUP ROUTES ////
 
-app.get("/", (req, res) => {
-    const userTest = new models.Person({firstName : "Test", lastName : "testowski", birthDate: new Date(), phone: "+48123456789", email: `example${Math.random()}@example.com`})
-    userTest.save().then( () =>  {
-        
-        res.send("express server works!")
-    }).catch(() => {
-        res.send("unable to save model")
+routes.hotel(app)
+routes.person(app)
+routes.session(app)
+routes.reservation(app)
+
+//// SETUP SERVER WITH DATABASE ////
+
+utils.connect(() => {
+    //// LISTENER ////
+    app.listen(PORT, () => {
+        console.log(`server started @ http://localhost:${PORT}`)
     })
 })
 
-//// HOTEL ////
-
-app.get("/hotel/:id", (req, res) => {
-    res.send(`/hotel/${req.params.id}`)
-})
-
-app.post("/hotel", (req, res) => {
-    res.send("/hotel")
-})
-
-//// ROOM ////
-
-app.get("/room/:id", (req, res) => {
-    res.send(`/room/${req.params.id}`)
-})
-
-app.post("/room", (req, res) => {
-    res.send("/room")
-})
-
-//// RESERVATIONS ////
-
-app.get("/reservations", (req, res) => {
-    res.send("/reservations")
-})
-
-app.get("/reservation/:id", (req, res) => {
-    res.send(`/reservation/${req.params.id}`)
-})
-
-app.post("/reservation", (req, res) => {
-    res.send(`/reservation`)
-})
-
-
-//// LISTENER ////
-
-app.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`)
+app.get("/", (req, res) => {
+    res.send("Server works!")
 })
