8484ba97af8117bb67a10a1d356e7e728d95b2ef
Nepommuck <pawel.konopka5@gmail.com>
2023-06-20 01:38:20 +0200

    Modify room form

--- client/src/pages/ManagerView/ManagerView.js
+++ client/src/pages/ManagerView/ManagerView.js
@@ -1,9 +1,14 @@
 import {React, useEffect, useState} from 'react';
 import './managerView.css'
+import ModifyForm from '../ModifyForm/ModifyForm';
 
 const ManagerView = () => {
   const [hotels, setHotels] = useState([]);
   const [isDisplayed, setDisplayed] = useState([]);
+  const [isFormOpen, setFormOpen] = useState(false);
+
+  const [modifiedRoom, setModifiedRoom] = useState();
+  const [modifiedHotel, setModifiedHotel] = useState();
 
   useEffect(() => {
     const fetchHotels = async () => {
@@ -34,44 +39,64 @@ const ManagerView = () => {
 
   const modifyRoom = (hotel, room) => {
     console.log("Modifying room unimplemented.")
+    setModifiedRoom(room)
+    setModifiedHotel(hotel)
+    setFormOpen(true)
   }
 
   const addNewRoom = (hotel) => {
     console.log("Adding room unimplemented.")
   }
 
+  const handleModifyRoomSubmit = (output) => {
+    console.log("Handling room modification unimplemented. (From "+modifiedRoom.beds+" to "+output.newBedsNumber+")")
+    setFormOpen(false)
+  }
+
+  var returnValue
+  if (isFormOpen)
+    returnValue = <ModifyForm 
+    type="modify-room" 
+    onSubmit={handleModifyRoomSubmit}
+    hotel={modifiedHotel} 
+    room={modifiedRoom}>
+    </ModifyForm>
+    // returnValue = <h2>Test</h2>
+  else
+    returnValue = 
+      <div className="manager-view">
+        <h1>Widok menedżera</h1>
+        <ul class="hotels-list">
+            {hotels?.map((hotel, index) => (
+              <li>{hotel.Id}{hotel.name}
+
+                {!isDisplayed[index] &&
+                <button onClick={() => swapHotelDisplay(hotel, index)}>Rozwiń szczegóły</button>}
+
+                {isDisplayed[index] &&
+                <div>
+                  <button onClick={() => swapHotelDisplay(hotel, index)}>Schowaj szczegóły</button>
+                  <button class="modify" onClick={() => addNewRoom(hotel, index)}>Dodaj pokój</button>
+                </div>}
+
+                {isDisplayed[index] &&
+                <ul class="rooms-list">
+                  {hotel.rooms.map((room, index) => (
+                    <li>
+                      Pokój <b>{room.internalNumber}</b>, liczba miejsc: <b>{room.beds}</b>
+                      <button class="remove" onClick={() => removeRoom(hotel, room)}>Usuń</button>
+                      <button class="modify" onClick={() => modifyRoom(hotel, room)}>Modyfikuj</button>
+                    </li>
+                  ))}
+                </ul>}
+                <hr></hr>
+              </li>
+            ))}
+        </ul>
+      </div>
+  
   return (
-    <div className="manager-view">
-      <h1>Widok menedżera</h1>
-      <ul class="hotels-list">
-          {hotels?.map((hotel, index) => (
-            <li>{hotel.Id}{hotel.name}
-
-              {!isDisplayed[index] &&
-              <button onClick={() => swapHotelDisplay(hotel, index)}>Rozwiń szczegóły</button>}
-
-              {isDisplayed[index] &&
-              <div>
-                <button onClick={() => swapHotelDisplay(hotel, index)}>Schowaj szczegóły</button>
-                <button class="modify" onClick={() => addNewRoom(hotel, index)}>Dodaj pokój</button>
-              </div>}
-
-              {isDisplayed[index] &&
-              <ul class="rooms-list">
-                {hotel.rooms.map((room, index) => (
-                  <li>
-                    Pokój <b>{room.internalNumber}</b>, liczba miejsc: <b>{room.beds}</b>
-                    <button class="remove" onClick={() => removeRoom(hotel, room)}>Usuń</button>
-                    <button class="modify" onClick={() => modifyRoom(hotel, room)}>Modyfikuj</button>
-                  </li>
-                ))}
-              </ul>
-              }
-              <hr></hr>
-            </li>
-          ))}
-      </ul>
-    </div>
+    returnValue
   );
 };
 
